177ae5ded05b51e5d0d334e66956982782368349
Ken-S-0114 <sato.ken@moneyforward.co.jp>
2019-05-09 18:08:02 +0900

    Implementation Search function

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,6 +9,15 @@ class Task < ApplicationRecord
 
   scope :recent, -> { order(created_at: :desc) }
   
+  # 検索結果を絞る
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
+
   private
 
   def validate_name_not_including_comma
